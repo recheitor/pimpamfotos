fc60ee46c556b1d07c30ccd8ba7b799bc2c26270
Alejandro Reche Costa <alejandro@MacBook-Pro-de-Alejandro-2.local>
2025-02-12 20:29:07 +0100

    marging

--- components/services/hero.tsx
+++ components/services/hero.tsx
@@ -5,10 +5,12 @@ interface ServicesHeroProps {
 export default function ServicesHero({ lang }: ServicesHeroProps) {
   return (
     <section className="relative h-[200px] bg-gradient-to-r from-primary to-primary flex items-center justify-center text-white">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4">{lang === "es" ? "NUESTROS FOTOMATONES" : "OUR PHOTOBOOTHS"}</h1>
-        <p className="text-xl mb-8">{lang === "es" ? "Elegantes y rápidos" : "Elegant and fast"}</p>
-      </div>
+      <div className="md:px-0 px-4">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold mb-4">{lang === "es" ? "NUESTROS FOTOMATONES" : "OUR PHOTOBOOTHS"}</h1>
+          <p className="text-xl mb-8">{lang === "es" ? "Elegantes y rápidos" : "Elegant and fast"}</p>
+        </div>
+      </div>  
     </section>
   )
 }
